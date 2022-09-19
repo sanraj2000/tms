da36804448bb5a2c6fdfb1992003a3f34320f7db
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-09-19 14:34:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -159,5 +159,33 @@ console.log("started");
             }
         });
 
+     //tabs
+          function connectorTabs(tabName) {
+            var i;
+            var x = document.getElementsByClassName("wmiotabtype");
+            for (i = 0; i < x.length; i++) {
+              x[i].style.display = "none";
+            }
+            document.getElementById(tabName).style.display = "block";
+
+            if(tabName === "Schedules"){
+              document.getElementById('defaultOpen2').click();
+            }
+          }
+          //// Get the element with id="defaultOpen" and click on it
+          //  document.getElementById("defaultOpen").Click();
+
+          
+          //tabs schedules
+          function connectorTabs2(tabName) {
+            var i;
+            var x = document.getElementsByClassName("wmiotabtype2");
+            for (i = 0; i < x.length; i++) {
+              x[i].style.display = "none";
+            }
+            document.getElementById(tabName).style.display = "block";
+           
+     
+
 
    
