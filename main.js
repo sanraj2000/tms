c904394a411e36213af17bb0a3a6c10370ebd1a7
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-09-21 14:25:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -192,5 +192,15 @@ console.log("started");
           //// Get the element with id="defaultOpen" and click on it
           //  document.getElementById("defaultOpen").Click();
 
-          
+           //tabs schedules
+          function connectorTabs2(tabName) {
+            var i;
+            var x = document.getElementsByClassName("wmiotabtype2");
+            for (i = 0; i < x.length; i++) {
+              x[i].style.display = "none";
+            }
+            document.getElementById(tabName).style.display = "block";
+           
+          }
+       
    
